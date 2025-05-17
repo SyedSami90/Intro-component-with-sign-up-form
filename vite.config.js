243522c4f2132d41472b1273a3b83ce370eebdab
SyedSami90 <syedzaman8989@gmail.com>
2025-05-17 05:32:33 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  ],
-  base: '/Intro-component-with-sign-up-form/',
-  build: {
-    assetsInlineLimit: 0  // Force JS/CSS to be separate files
-  }
+  ]
 })
